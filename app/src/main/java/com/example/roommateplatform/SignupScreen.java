538f50dfe55b28414489b26e6511f29d685df85f
Hwangmouse <0327jieun@gachon.ac.kr>
2025-05-18 02:11:31 +0900

    회원가입이랑 로그인

---
+++ app/src/main/java/com/example/roommateplatform/SignupScreen.java
@@ -0,0 +1,15 @@
+package com.example.roommateplatform;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SignupScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.signupscreen);
+
+    }
+}
